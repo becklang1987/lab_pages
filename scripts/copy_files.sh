27c118f2df66ea5c6f727b22845bfa4e1c876cd0
becklang1987 <119975424+becklang1987@users.noreply.github.com>
2024-07-14 20:02:05 +0800

    22th_commit

--- scripts/copy_files.sh
+++ scripts/copy_files.sh
@@ -2,8 +2,8 @@
 
 cp kupotech.com.cn.pem /etc/certs
 cp kupotech.com.cn.key /etc/certs
-cp sd_page /home/ubuntu/data/www
-cp nginx_custom.conf /etc/nginx/custom_configs
+cp -r sd_page /home/ubuntu/data/www
+cp scripts/nginx_custom.conf /etc/nginx/custom_configs
 
 CONFIG_FILE="/etc/nginx/nginx.conf"
 INSERT_LINE="include /etc/nginx/custom_configs/nginx_custom.conf;"
@@ -12,6 +12,6 @@ SEARCHED_LINE="include /etc/nginx/sites-enabled/*;"
 if ! grep -q "$INSERT_LINE" $CONFIG_FILE; then
     sed -i "/$SEARCHED_LINE/a \ include /etc/nginx/custom_configs/nginx_custom.conf;" $CONFIG_FILE
     echo "Added custom config to nginx.conf"
-esle
+else
     echo "Custom config already exists in nginx.conf"
 fi
