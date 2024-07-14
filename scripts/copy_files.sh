ea50654d51d4e8eb55289170f4a5ee3578c37f48
becklang1987 <119975424+becklang1987@users.noreply.github.com>
2024-07-14 19:38:58 +0800

    20th_commit

--- scripts/copy_files.sh
+++ scripts/copy_files.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-mkdir /etc/certs
-cp kupotech.com.cn.pem /etc/certs/
-cp kupotech.com.cn.key /etc/certs/
-mkdir /home/ubuntu/data/www
-cp sd_page /home/ubuntu/data/www/
-mkdir /etc/nginx/custom_configs
-cp nginx.cnf /etc/nginx/custom_configs/
+cp kupotech.com.cn.pem /etc/certs
+cp kupotech.com.cn.key /etc/certs
+cp sd_page /home/ubuntu/data/www
+cp nginx_custom.conf /etc/nginx/custom_configs
 
 CONFIG_FILE="/etc/nginx/nginx.conf"
 INSERT_LINE="include /etc/nginx/custom_configs/nginx_custom.conf;"
